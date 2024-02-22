e128b2b0c2caee3724d1e7013c161b25e494a0e1
Javier Muñoz Tous <javiermunoz.tous@biogen.com>
2024-02-22 20:28:48 +0100

    add blog posts, categories route and tailwindcss

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,17 @@
 import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	type: "content",
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    shortDescription: z.string(),
+    subtitle: z.string(),
+    pubDate: z.coerce.date(),
+    draft: z.boolean().optional(),
+    updatedDate: z.coerce.date().optional(),
+    thumbnail: z.string().optional(),
+  }),
 });
 
 export const collections = { blog };
