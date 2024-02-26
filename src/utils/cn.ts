1af0ceb51bf097545f038aa85245b89f57bf6726
Javier Muñoz Tous <javimtib92@gmail.com>
2024-02-26 12:51:40 +0100

    remove tw-merge dependency

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,6 +1,5 @@
 import { type ClassValue, clsx as lite } from 'clsx';
-import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(lite(inputs));
+  return lite(inputs);
 }
