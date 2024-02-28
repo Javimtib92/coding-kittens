12be8db0f5a4f8ef757bf19f6bee7368b097d0e2
Javier Muñoz Tous <javimtib92@gmail.com>
2024-02-28 19:31:30 +0100

    fix image optimization

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,15 +3,16 @@ import { defineCollection, z } from 'astro:content';
 const blog = defineCollection({
   type: 'content',
   // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    shortDescription: z.string(),
-    subtitle: z.string(),
-    pubDate: z.coerce.date(),
-    draft: z.boolean().optional(),
-    updatedDate: z.coerce.date().optional(),
-    thumbnail: z.string().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      shortDescription: z.string(),
+      subtitle: z.string(),
+      pubDate: z.coerce.date(),
+      draft: z.boolean().optional(),
+      updatedDate: z.coerce.date().optional(),
+      thumbnail: image(),
+    }),
 });
 
 export const collections = { blog };
