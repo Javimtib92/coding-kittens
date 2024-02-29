ff0920ca2290db805b34fe4dd452ee776354e0c0
Javier Muñoz Tous <javimtib92@gmail.com>
2024-02-29 20:24:36 +0100

    upgrade shiki

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,35 +3,33 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import catppuccinLate from './shiki/themes/catppuccin-late.mjs';
-import catppuccinMocha from './shiki/themes/catppuccin-mocha.mjs';
+import catppuccinLate from '@catppuccin/vscode/themes/latte.json';
+import catppuccinMocha from '@catppuccin/vscode/themes/mocha.json';
 import { remarkReadingTime } from './remark/remark-reading-time.mjs';
+import rehypePrettyCode from 'rehype-pretty-code';
 
-// import {} from // transformerNotationFocus,
-// // transformerNotationDiff,
-// '@shikijs/transformers';
+import {
+  transformerNotationFocus,
+  transformerNotationDiff,
+} from 'shikiji-transformers';
+
+const prettyCodeOptions = {
+  keepBackground: false,
+  theme: {
+    dark: catppuccinMocha,
+    light: catppuccinLate,
+  },
+  defaultLang: 'jsx',
+  transformers: [transformerNotationFocus(), transformerNotationDiff()],
+};
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://coding-kittens.com',
   integrations: [mdx(), sitemap(), tailwind(), react()],
   markdown: {
-    shikiConfig: {
-      theme: {},
-      experimentalThemes: {
-        light: {
-          ...catppuccinLate,
-          bg: '#FAFAF9',
-          fg: '#FAFAF9',
-        },
-        dark: {
-          ...catppuccinMocha,
-          bg: '#181B2D',
-          fg: '#181B2D',
-        },
-      },
-      transformers: [],
-    },
+    syntaxHighlight: false,
     remarkPlugins: [remarkReadingTime],
+    rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
   },
 });
