cc70bd59dafd37aa677d571ebf1e1a8c526a3c0d
Javier Muñoz Tous <javimtib92@gmail.com>
2024-03-19 12:48:55 +0100

    translate hardcoded text, dates and reading time

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -10,13 +10,25 @@ export const ui = {
   en: {
     'nav.home': 'About Me',
     'nav.blog': 'Blog',
+    'latest.title': 'The Latest Posts',
+    'latest.link': 'Browse all posts',
+    'categoryList.all': 'All',
+    'footer.createdBy': 'Created by',
   },
   es: {
     'nav.home': 'Sobre mí',
     'nav.blog': 'Blog',
+    'latest.title': 'Los posts más recientes',
+    'latest.link': 'Descubre todos los posts',
+    'categoryList.all': 'Todo',
+    'footer.createdBy': 'Creado por',
   },
   ca: {
     'nav.home': 'Sobre mi',
     'nav.blog': 'Blog',
+    'latest.title': 'Els posts més recents',
+    'latest.link': 'Descobreix tots els posts',
+    'categoryList.all': 'Tot',
+    'footer.createdBy': 'Creat per',
   },
 } as const;
