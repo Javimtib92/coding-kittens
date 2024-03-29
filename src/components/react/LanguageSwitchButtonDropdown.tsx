1ba22a166e54c920d02daeeffa27059ad5a49ec5
Javier Muñoz Tous <javimtib92@gmail.com>
2024-03-29 02:43:05 +0100

    replace dropdown for aria components

--- src/components/react/LanguageSwitchButtonDropdown.tsx
+++ src/components/react/LanguageSwitchButtonDropdown.tsx
@@ -1,16 +1,33 @@
-import { DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu';
 import { Languages, Check } from 'lucide-react';
+
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-} from '~/components/react/DropdownMenu';
+  Button,
+  Menu,
+  MenuItem,
+  MenuTrigger,
+  Popover,
+} from 'react-aria-components';
+import { cn } from '~/utils/cn';
 
 export function LanguageSwitchButtonDropdown({
   locale,
 }: {
   locale: string | undefined;
 }) {
+  const items = [
+    {
+      id: 'en',
+      name: 'English',
+    },
+    {
+      id: 'es',
+      name: 'Español',
+    },
+    {
+      id: 'ca',
+      name: 'Català',
+    },
+  ];
   const onChangeLanguage = (locale: string) => {
     const url = new URL(window.location.href);
 
@@ -22,34 +39,34 @@ export function LanguageSwitchButtonDropdown({
   };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <button
-          aria-label='Switch language button'
-          className='-mx-4 -my-2 px-4 py-2'
-        >
-          <span className='h-6 w-6 text-primary-600 dark:text-primary-100'>
-            <Languages />
-          </span>
-        </button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuItem onClick={() => onChangeLanguage('en')}>
-          <div className='flex flex-row items-center justify-between gap-2'>
-            English {locale === 'en' && <Check className='w-4' />}
-          </div>
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => onChangeLanguage('ca')}>
-          <div className='flex flex-row items-center justify-between gap-2'>
-            Català {locale === 'ca' && <Check className='w-4' />}
-          </div>
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => onChangeLanguage('es')}>
-          <div className='flex flex-row items-center justify-between gap-2'>
-            Español {locale === 'es' && <Check className='w-4' />}
-          </div>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <MenuTrigger>
+      <Button
+        aria-label='Switch language button'
+        className='-mx-4 -my-2 px-4 py-2'
+      >
+        <span className='h-6 w-6 text-primary-600 dark:text-primary-100'>
+          <Languages />
+        </span>
+      </Button>
+      <Popover
+        className={cn(
+          'bg-popover text-popover-foreground z-50 min-w-[8rem] overflow-hidden rounded-md border bg-white p-1 shadow-md dark:bg-background-700'
+        )}
+      >
+        <Menu items={items} onAction={(id) => onChangeLanguage(id as string)}>
+          {(item) => (
+            <MenuItem
+              className={cn(
+                'relative flex cursor-pointer select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors'
+              )}
+            >
+              <div className='flex flex-row items-center justify-between gap-2'>
+                {item.name} {locale === item.id && <Check className='w-4' />}
+              </div>
+            </MenuItem>
+          )}
+        </Menu>
+      </Popover>
+    </MenuTrigger>
   );
 }
